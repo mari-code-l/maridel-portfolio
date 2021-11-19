7badd996fedea9e09cb669825ec2e1f54558f0f3
mari-code-l <maridel.arcagua@gmail.com>
2021-11-19 15:42:42 +0000

    Added emailjs functionatilty

---
+++ sendEmail.js
@@ -0,0 +1,20 @@
+function sendMail(contactForm) {
+    emailjs.send("service_web","template_gj1sxvn", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    
+    .then(
+        function(response) {
+        alert("Submitted!");
+        contactForm.name.value = '';
+        contactForm.email.value = '';
+        contactForm.projectsummary.value = '';
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;
+}
