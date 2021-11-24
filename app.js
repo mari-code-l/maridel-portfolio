cecc014e739d965f488150c65b0d8237e0120472
mari-code-l <maridel.arcagua@gmail.com>
2021-11-24 20:20:26 +0000

    Add Form functionality

--- app.js
+++ app.js
@@ -110,4 +110,12 @@
       $("li", "#places" ).eq( 0 ).trigger( "click");
     });
   })(jQuery);
+
+
+let form = document.getElementsByTagName("form")[0];
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("Form Submitted!");
+});
+
   
